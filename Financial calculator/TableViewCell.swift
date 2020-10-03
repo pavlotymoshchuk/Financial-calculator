2b9e4b400fe12eeed157ca35ce9943fbd3e66131
Pavlo Tymoshchuk <Pavlo25575@icloud.com>
2020-10-03 13:21:50 +0300

    Added struct Deposit, connected it to TableView

---
+++ Financial calculator/TableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TableViewCell.swift
+//  Financial calculator
+//
+//  Created by Павло Тимощук on 03.10.2020.
+//  Copyright © 2020 Павло Тимощук. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var indexLabel: UILabel!
+    @IBOutlet weak var dateStartLabel: UILabel!
+    @IBOutlet weak var dateEndLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+    @IBOutlet weak var presentValueLabel: UILabel!
+    @IBOutlet weak var futureValueLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Financial calculator/ViewController.swift
+++ Financial calculator/ViewController.swift
@@ -8,11 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    struct Term {
+        var dateStart: Int //Date
+        var dateEnd: Int //Date
+        var percentage: Double
+    }
+    
+    struct Deposit {
+        var presentValue: Double?
+        var futureValue: Double?
+        var termsAndPercentages: [Term]
+    }
+    
+    var depositsArray: [Deposit] = []
+    
+    @IBOutlet weak var tableDeposits: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let term1 = Term(dateStart: 15, dateEnd: 17, percentage: 15)
+        let term2 = Term(dateStart: 18, dateEnd: 22, percentage: 19)
+        depositsArray.append(Deposit(presentValue: 100000, futureValue: nil, termsAndPercentages: [term1]))
+        depositsArray.append(Deposit(presentValue:  nil, futureValue: 300000, termsAndPercentages: [term1,term2]))
+    }
+    
+    // MARK: - Число всіх рядків (numberOfRowsInSection)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return depositsArray.count
+    }
+    
+    // MARK: - Заповнення рядків (cellForRowAt)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as? TableViewCell
+        {
+            let item = depositsArray[indexPath.row]
+            
+            if item.termsAndPercentages.count > 1 { // MARK: - Більше одного терміна
+                cell.indexLabel?.text = String(indexPath.row+1)
+                cell.dateStartLabel?.text = String(item.termsAndPercentages[0].dateStart)
+                cell.dateEndLabel?.text = String(item.termsAndPercentages[item.termsAndPercentages.count-1].dateEnd)
+                cell.percentageLabel?.text = "%" // MARK: - Обчислити середню ставку відсотків
+                cell.presentValueLabel?.text = (item.presentValue != nil) ? "PV="+String(item.presentValue!) : "undefined"
+                cell.futureValueLabel?.text = (item.futureValue != nil) ? "FV="+String(item.futureValue!) : "undefined"
+            } else if item.termsAndPercentages.count == 1 { // MARK: - Один термін
+                cell.indexLabel?.text = String(indexPath.row+1)
+                cell.dateStartLabel?.text = String(item.termsAndPercentages[0].dateStart)
+                cell.dateEndLabel?.text = String(item.termsAndPercentages[0].dateEnd)
+                cell.percentageLabel?.text = String(item.termsAndPercentages[0].percentage)+"%"
+                cell.presentValueLabel?.text = (item.presentValue != nil) ? "PV="+String(item.presentValue!) : "undefined"
+                cell.futureValueLabel?.text = (item.futureValue != nil) ? "FV="+String(item.futureValue!) : "undefined"
+            } else {
+                // BUG
+                print("!!!BUG!!!")
+            }
+            
+            return cell
+        }
+        return UITableViewCell()
     }
 
 
